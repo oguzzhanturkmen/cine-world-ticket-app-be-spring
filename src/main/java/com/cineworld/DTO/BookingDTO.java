90f637ea9d1b6ea4f41b1a8f818bdc19994b99e6
Oguzhan T <oguzzhanturkmen@gmail.com>
2023-12-08 04:52:40 +0300

    Booking Service is done git add .

--- src/main/java/com/cineworld/DTO/BookingDTO.java
+++ src/main/java/com/cineworld/DTO/BookingDTO.java
@@ -1,4 +1,17 @@
 package com.cineworld.DTO;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
 public class BookingDTO {
+
+
+    private Integer totalPrice;
+    private Integer userId;
+    private Integer paymentDetailId;
+    private Integer showTimeId;
+    private List<SeatDTO> seats;
 }
---
+++ src/main/java/com/cineworld/DTO/SeatDTO.java
@@ -0,0 +1,10 @@
+package com.cineworld.DTO;
+
+import lombok.Data;
+
+@Data
+public class SeatDTO {
+
+    private Long seatId;
+
+}
